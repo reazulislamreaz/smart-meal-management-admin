83cf5d947758c8c28b9f684134040e88e259899b
reazulislam1487 <reazulislam1487@gmail.com>
2026-06-25 15:36:37 +0600

    build(config): configure vite server and preview settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  preview: {
+    host: "206.162.244.11",
+    port: 9173,
+    strictPort: true,
+  },
+  server: {
+    port: 3000, // Your desired port
+    strictPort: true, // Prevent Vite from choosing another port if 3000 is busy
+  },
+});
